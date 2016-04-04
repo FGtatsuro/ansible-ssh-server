284dd68ce11686845af66f56e2affa581ba87b84
FGtatsuro <fujiistorage@gmail.com>
2016-04-04 15:37:34 +0900

    Add specs for Alpine Linux.

--- spec/ssh_server_spec.rb
+++ spec/ssh_server_spec.rb
@@ -4,6 +4,10 @@ describe package('openssh-server'), :if => ['debian'].include?(os[:family]) do
   it { should be_installed }
 end
 
+describe package('openssh'), :if => ['alpine'].include?(os[:family]) do
+  it { should be_installed }
+end
+
 describe command('which sshd') do
   its(:exit_status) { should eq 0 }
 end
